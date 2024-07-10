e2fe4f417829351dd7770281d628e23b745972c8
Brent Reynolds <brentsreynolds5@gmail.com>
2024-07-09 22:04:01 -0400

    Changed Airport fields to nullable

--- api/api/Models/Airport.cs
+++ api/api/Models/Airport.cs
@@ -7,18 +7,18 @@ namespace api.Models
     public class Airport
     {
         public int AirportId { get; set; }
-        public string Name { get; set; }
-        public string City { get; set; }
-        public string Country { get; set; }
-        public string IATA { get; set; }
-        public string ICAO { get; set; }
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
-        public int Altitude { get; set; }
-        public double TimeZone { get; set; }
-        public string DST { get; set; } // Day Light Savings info
-        public string TzDatabaseTimeZone { get; set; }
-        public string Type { get; set; } // Type of airport
-        public string Source { get; set; } // Source of this data
+        public string? Name { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
+        public string? IATA { get; set; }
+        public string? ICAO { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+        public int? Altitude { get; set; }
+        public double? TimeZone { get; set; }
+        public string? DST { get; set; } // Day Light Savings info
+        public string? TzDatabaseTimeZone { get; set; }
+        public string? Type { get; set; } // Type of airport
+        public string? Source { get; set; } // Source of this data
     }
 }
