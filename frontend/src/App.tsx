90780003f28f5669a03f876e9599562e39f5aa74
Brent Reynolds <brentsreynolds5@gmail.com>
2024-07-13 09:55:00 -0400

    Added auth services

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 import './App.css';
 import CardList from './Components/CardList/CardList';
 import Search from './Components/Search/Search';
@@ -7,6 +9,7 @@ function App() {
     <div className="App">
      <Search />
      <CardList />
+     <ToastContainer />
     </div>
   );
 }
